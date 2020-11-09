94f84735ac6502c5d2418996bd51b8015f608f2b
Manukonda Renee Suresh <61098701+renees21@users.noreply.github.com>
2020-11-09 22:17:26 +0530

    Commit message

---
+++ sqlScripts/atmDatabase.sql
@@ -0,0 +1,32 @@
+-- create table CustDetails(
+-- CAccNo varchar(255),
+-- Cname varchar(255),
+-- AccBalance float,
+-- AccDeposit float, 
+-- AccWithdraw float
+-- )
+
+-- insert into CustDetails(CAccNo, Cname, AccBalance) values ("1123837ETB", "Jane Solano", "40000");
+-- insert into CustDetails(CAccNo, Cname, AccBalance) values ("1123838EUC", "Rafael Solano", "40000");
+-- insert into CustDetails(CAccNo, Cname, AccBalance) values ("1123WTCW60", "Michael Cordero", "25000");
+-- insert into CustDetails(CAccNo, Cname, AccBalance) values ("11238TXOMA", "Rogelio De La Vega", "80000");
+-- insert into CustDetails(CAccNo, Cname, AccBalance) values ("11238QMNSD", "Xiomara De La Vega", "40000");
+
+-- alter table CustDetails add password varchar(255);
+
+-- insert into CustDetails(password) values ("Jane1")
+--  insert into CustDetails(password) values ("RafMateo2")
+ -- insert into CustDetails(password) values ("Charlie3");
+--  insert into CustDetails(password) values ("XiomaraBeb");
+ -- insert into CustDetails(password) values ("XoRoAlba");
+ 
+-- select * from CustDetails
+
+-- update CustDetails
+-- set password="Jane1" 
+-- where CAccNo = "1123837ETB";
+ -- delete from CustDetails where password = "Jane1";
+
+-- update CustDetails
+-- set password="XoRoAlba" 
+-- where CAccNo = "11238QMNSD";
